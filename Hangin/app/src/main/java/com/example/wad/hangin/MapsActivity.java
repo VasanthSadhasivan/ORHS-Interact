9d3d0fd9ddd5392df86271bff5e5224754cd9e72
Vasanth Sadhasivan <vasanthsadhasivan@gmail.com>
2016-03-26 22:04:02 -0700

    AYY BB

--- Hangin/app/src/main/java/com/example/wad/hangin/MapsActivity.java
+++ Hangin/app/src/main/java/com/example/wad/hangin/MapsActivity.java
@@ -1,21 +1,24 @@
 package com.example.wad.hangin;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements
         OnConnectionFailedListener {
@@ -23,12 +26,17 @@ public class MapsActivity extends FragmentActivity implements
     private static final String TAG = "MapsActivity";
     private boolean mResolvingError = false;
     private GoogleApiClient mGoogleApiClient;
+
     private static final String STATE_RESOLVING_ERROR = "resolving_error";
     int PLACE_PICKER_REQUEST = 1;
 
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
         setContentView(R.layout.activity_maps);
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
@@ -42,6 +50,7 @@ public class MapsActivity extends FragmentActivity implements
         mResolvingError = savedInstanceState != null
                 && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
         try {
             startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
         } catch (GooglePlayServicesRepairableException e) {
@@ -50,17 +59,36 @@ public class MapsActivity extends FragmentActivity implements
             e.printStackTrace();
         }
 
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "PlaceName: " + place.getName() +"\n PlaceLatLng: "+place.getLatLng()+"\n PlaceAddress: "+place.getAddress());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
     }
-    
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
                 Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
+                String toastMsg = "PlaceName: " + place.getName() +"\n PlaceLatLng: "+place.getLatLng()+"\n PlaceAddress: "+place.getAddress();
                 Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
             }
         }
     }
+
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
