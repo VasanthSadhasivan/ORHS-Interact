87eeecd2048c4920ea8709c08b1e2b5bc1f30eda
Vasanth Sadhasivan <vasanthsadhasivan@gmail.com>
2016-08-20 20:21:59 -0700

    AYY BOIIS

--- ORHS Interact/app/src/main/java/com/teacupofcode/ved/interactapp/MainActivity.java
+++ ORHS Interact/app/src/main/java/com/teacupofcode/ved/interactapp/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends Activity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w("shit","sdkjshdjklasdhjkasdhJKLasdhJKLASDHjkasdjklHASDJKLASDjklh");
+
         if (savedInstanceState != null) {
             mIsResolving = savedInstanceState.getBoolean(KEY_IS_RESOLVING);
             mShouldResolve = savedInstanceState.getBoolean(KEY_SHOULD_RESOLVE);
